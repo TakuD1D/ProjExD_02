f0ae62073a7561dfa45e4c6a69ab3fdddeb64ea4
TakuD1D <c0a2205191@edu.teu.ac.jp>
2023-10-03 17:23:37 +0900

    エラーだらけ

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,9 +12,8 @@ delta = {  # 移動量の辞書
     pg.K_RIGHT: (5, 0),
 }
 
+
 """ 画面外判定 """
-
-
 def check_bound(img_rect: pg.Rect) -> tuple:
     """
     引数：こうかとん、爆弾Rect
@@ -30,6 +29,7 @@ def check_bound(img_rect: pg.Rect) -> tuple:
     if img_rect.top < 0 or HEIGHT < img_rect.bottom:
         height_check = False
     return width_check, height_check
+    
 
 
 def main():
@@ -42,18 +42,38 @@ def main():
     # こうかとん画
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_img_flip = pg.transform.flip(kk_img, True, False)
     kk_rect = kk_img.get_rect()
     kk_rect.center = (900, 400)
+    kk_dic = { # こうかとんいろんな角度
+        (0, 0): kk_img,
+        (0, -5) : pg.transform.rotozoom(kk_img_flip, 90, 1.0),
+        (5, -5) : pg.transform.rotozoom(kk_img_flip, 45, 1.0),
+        (5, 0) : pg.transform.rotozoom(kk_img_flip, 0, 1.0),
+        (5, 5) : pg.transform.rotozoom(kk_img_flip, -45, 1.0),
+        (0, 5) : pg.transform.rotozoom(kk_img_flip, -90, 1.0),
+        (-5, 5) : pg.transform.rotozoom(kk_img, 45, 1.0),
+        (-5, 0) : pg.transform.rotozoom(kk_img, 0, 1.0),
+        (-5, -5) : pg.transform.rotozoom(kk_img, -45, 1.0),
+        
+    }
+
     
 
     """ 爆弾サークル """
-    enn = pg.Surface((20, 20))
-    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
-    enn.set_colorkey((0, 0, 0))  # 透明化のためのset_colorkey
+    bb_imgs = []
+    for r in range(1, 11):
+        enn = pg.Surface((20*r, 20*r))
+        pg.draw.circle(enn, (255, 0, 0), (10*r, 10*r), 10*r)
+        enn.set_colorkey((0, 0, 0))
+        bb_imgs.append(enn)
+      # 透明化のためのset_colorkey
     img_baku = enn.get_rect()
     x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     img_baku.center = (x, y)
     vx, vy = +5, +5  # 爆弾移動量
+    accs = [a for a in range(1,11)]
+    
 
     """ 設定 """
     clock = pg.time.Clock()
@@ -78,19 +98,25 @@ def main():
             if key_lst[key]:
                 sum_move[0] += value[0]  # 横方向
                 sum_move[1] += value[1]  # 縦方向
+            
         kk_rect.move_ip(sum_move[0], sum_move[1])
         if check_bound(kk_rect) != (True, True):
             kk_rect.move_ip(-sum_move[0], -sum_move[1])
+        for key_, value_ in kk_dic.items():
+                if key_ == tuple(sum_move):
+                    kk_img = value_
         screen.blit(kk_img, kk_rect)  # 900, 400
 
         """爆弾"""
-        img_baku.move_ip(vx, vy)
+        avx, avy = vx*accs[min(tmr//500, 9)], vy*accs[min(tmr//500, 9)]
+        bb_img = bb_imgs[min(tmr//500, 9)]
+        img_baku.move_ip(avx, avy)
         yoko, tate = check_bound(img_baku)
         if not yoko:  # 横方向はみ出し
             vx *= -1
         if not tate: # 縦方向はみ出し
             vy *= -1
-        screen.blit(enn, img_baku)  # enn-> 円 img_baku->座標を設定
+        screen.blit(bb_img, img_baku)  # enn-> 円 img_baku->座標を設定
 
         """ 設定 """
         pg.display.update()
